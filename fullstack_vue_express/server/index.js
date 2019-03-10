16ac039f5dd2252314af996fdfd0c51db765a40d
Andrew G <booleanorama@gmail.com>
2019-03-10 01:29:36 -0500

    client side

--- fullstack_vue_express/server/index.js
+++ fullstack_vue_express/server/index.js
@@ -11,6 +11,6 @@ const posts = require('./routes/api/posts');
 
 app.use('/api/posts', posts);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5050;
 
 app.listen(port, () => console.log (`Server started on port ${port}`));
