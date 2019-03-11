0f3daaf55484a03fd2c6d713c45bfdcecb87cf8f
Andrew G <booleanorama@gmail.com>
2019-03-11 10:13:08 -0400

    Vues initialized

---
+++ vue_crash_todolist/src/components/Todos.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <div v-bind:key="todo.id" v-for="todo in todos">
+       <Todoitem v-bind:todo="todo" />
+    </div>
+</div>
+</template>
+
+<script>
+import Todoitem from './Todoitem.vue';
+
+export default {
+    name: "Todos",
+    components: {
+        Todoitem
+    },
+    props: ["todos"]
+}
+</script>
+
+<style scoped>
+
+
+</style>
