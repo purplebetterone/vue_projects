3632a120fea6814efd5c5029d4ff1ffbf4495a24
Andrew G <booleanorama@gmail.com>
2019-03-11 23:33:53 -0400

    add with json placeholder

--- vue_crash_todolist/src/App.vue
+++ vue_crash_todolist/src/App.vue
@@ -11,6 +11,7 @@
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import axios from 'axios'
 
 
 export default {
@@ -23,21 +24,7 @@ export default {
   data() {
     return {
       todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false
-        },
-         {
-          id: 2,
-          title: "Todo Two",
-          completed: true
-        },
-         {
-          id: 3,
-          title: "Todo Three",
-          completed: false
-        },
+        
       ]
     }
   },
@@ -46,8 +33,22 @@ export default {
       this.todos = this.todos.filter(todo => todo.id !== id);
     },
     addTodo(newTodo){
-      this.todos = [...this.todos, newTodo];
+      const {title, completed} = newTodo;
+
+    axios.post('https://jsonplaceholder.typicode.com/todos', {
+      title, 
+      completed
+    })
+      .then(res =>  this.todos = [...this.todos, res.data])
+      .catch(err => console.log(error));
+
+      
     }
+  },
+  created(){
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+    .then(res => this.todos = res.data)
+    .catch(err => console.log(err));
   }
 }
 </script>
