27eb9683e3dfcafec4c5b19e2cf96b328076f109
Andrew G <booleanorama@gmail.com>
2019-03-12 23:40:31 -0400

    CJ's simple Tutorial

---
+++ simple_todo/app.js
@@ -0,0 +1,27 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        title: "Hello Coding Garden",
+        newTodo: '',
+        todos: []
+    },
+    methods:{
+        addTodo() {
+           // console.log(this.newTodo);
+           this.todos.push({
+               title: this.newTodo,
+               done: false
+           });
+           this.newTodo = '';
+        },
+        removeTodo(todo){
+            const todoIndex = this.todos.indexOf(todo);
+            this.todos.splice(todoIndex, 1);
+        }, 
+        allDone(){
+            this.todos.forEach(todo => {
+                todo.done = true;
+            })
+        }
+    }
+})
